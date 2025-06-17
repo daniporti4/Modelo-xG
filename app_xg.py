4f1b0a0035c51c7dcdf01b81091e669cd1d86594
daniporti4 <daniel.martin.perez4@gmail.com>
2025-06-17 18:07:27 +0200

    Update app_xg.py

--- app_xg.py
+++ app_xg.py
@@ -7,7 +7,6 @@ Original file is located at
     https://colab.research.google.com/drive/155aDzN1xZLSOGu_wgLtDzAxF9Daup2dK
 """
 
-#!pip install streamlit
 
 import streamlit as st
 import pandas as pd
